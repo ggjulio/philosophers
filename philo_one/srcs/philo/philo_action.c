3ec3b567e75a54bf7358e9f2212582bfc83713b0
ggjulio <juligonz@student.42.fr>
2021-01-13 11:17:43 +0100

    refacto again philo 1

--- philo_one/srcs/philo/philo_action.c
+++ philo_one/srcs/philo/philo_action.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 15:22:07 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/11 22:11:18 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/01/13 11:17:13 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,12 @@ void	philo_sleep(t_philo *philo)
 
 void	philo_eat(t_philo *philo)
 {
+	pthread_mutex_lock(&g_simu.lock);
 	pthread_mutex_lock(philo->right_fork);
 	print_message(philo, "has taken a fork", 0);
 	pthread_mutex_lock(philo->left_fork);
 	print_message(philo, "has taken a fork", 0);
+	pthread_mutex_unlock(&g_simu.lock);
 	set_action(philo, Action_Eat);
 	print_message(philo, "is eating", 0);
 	usleep_ms(g_simu.time_to_eat);
